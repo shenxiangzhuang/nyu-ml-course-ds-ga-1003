01410716aff754006b08fc14d96fd15b17d4566b
shenxiangzhuang <1021550072@qq.com>
2019-07-29 23:51:02 +0800

    hw1 finished

--- [2.9]Hw1/Answers/skeleton_code.py
+++ [2.9]Hw1/Answers/skeleton_code.py
@@ -44,7 +44,7 @@ def feature_normalization(train, test):
 # The square loss function
 def compute_square_loss(X, y, theta):
     """
-    Given a set of X, y, theta, compute the average square loss 
+    Given a set of X, y, theta, compute the average square loss
     for predicting y with X*theta.
 
     Args:
@@ -67,7 +67,7 @@ def compute_square_loss(X, y, theta):
     # Initialize the average square loss
     num_instances = X.shape[0]
     abs_loss = X @ theta - y
-    return (1 / num_instances) * (abs_loss.T @ abs_loss)
+    return (0.5 / num_instances) * (abs_loss.T @ abs_loss)
 
 
 #######################################
@@ -87,7 +87,7 @@ def compute_square_loss_gradient(X, y, theta):
         grad - gradient vector, 1D numpy array of size (num_features)
     """
     num_instances = X.shape[0]
-    return 1 / num_instances * (X @ theta - y).T @ X
+    return (1 / num_instances) * (X @ theta - y).T @ X
 
 
 #######################################
@@ -96,7 +96,8 @@ def compute_square_loss_gradient(X, y, theta):
 # of any gradient-based optimization algorithm. Fortunately, it's very
 # easy to check that the gradient calculation is correct using the
 # definition of gradient.
-# See http://ufldl.stanford.edu/wiki/index.php/Gradient_checking_and_advanced_optimization
+# See http://ufldl.stanford.edu/wiki/index.php/
+# Gradient_checking_and_advanced_optimization
 def grad_checker(X, y, theta, epsilon=0.01, tolerance=1e-4):
     """Implement Gradient Checker
     Check that the function compute_square_loss_gradient returns the
@@ -117,7 +118,8 @@ def grad_checker(X, y, theta, epsilon=0.01, tolerance=1e-4):
     distance exceeds tolerance, we say the gradient is incorrect.
 
     Args:
-        X - the feature vector, 2D numpy array of size (num_instances, num_features)
+        X - the feature vector, 2D numpy array
+            of size (num_instances, num_features)
         y - the label vector, 1D numpy array of size (num_instances)
         theta - the parameter vector, 1D numpy array of size (num_features)
         epsilon - the epsilon used in approximation
@@ -137,7 +139,9 @@ def grad_checker(X, y, theta, epsilon=0.01, tolerance=1e-4):
         approx_grad[feature_index] = \
             (compute_square_loss(X, y, theta + epsilon * h) -
              compute_square_loss(X, y, theta - epsilon * h)) / (2 * epsilon)
-    return np.allclose(true_gradient, approx_grad, atol=tolerance)
+    # return np.allclose(true_gradient, approx_grad, atol=tolerance)
+    distance = np.linalg.norm(approx_grad - true_gradient)
+    return distance < tolerance
 
 
 #######################################
@@ -166,7 +170,9 @@ def generic_gradient_checker(X,
         approx_grad[feature_index] = \
             (objective_func(X, y, theta + epsilon * h) -
              objective_func(X, y, theta - epsilon * h)) / (2 * epsilon)
-    return np.allclose(true_gradient, approx_grad, atol=tolerance)
+    # return np.allclose(true_gradient, approx_grad, atol=tolerance)
+    distance = np.linalg.norm(approx_grad - true_gradient)
+    return distance < tolerance
 
 
 #######################################
@@ -204,25 +210,41 @@ def batch_grad_descent(X, y, alpha=0.1, num_step=1000, grad_check=False):
         theta_hist[step_i, ] = theta
         loss_hist[step_i] = compute_square_loss(X, y, theta)
         if grad_check:
-            assert grad_checker(X, y, theta)
+            if not grad_checker(X, y, theta):
+                print(f"Gradiant Fails! \n Alpha: {alpha}; Step: {step_i}")
+                break
         theta -= compute_square_loss_gradient(X, y, theta) * alpha
     return theta_hist, loss_hist
 
 
-#######################################
-### Backtracking line search
-#Check http://en.wikipedia.org/wiki/Backtracking_line_search for details
-#TODO
+# plot - different alpha
+def step_size_plot(X, y):
+    # 0.5 will fail in gradient descent
+    # by gradient check
+    alphas = [0.01, 0.03, 0.05, 0.07, 0.1]
+    for alpha in alphas:
+        _, loss_hist = batch_grad_descent(X, y, alpha, num_step=2000)
+        plt.plot(loss_hist, label=f"Alpha={alpha}")
+    plt.legend()
+    plt.show()
 
 
 #######################################
-### The gradient of regularized batch gradient descent
+# Backtracking line search
+# Check http://en.wikipedia.org/wiki/Backtracking_line_search for details
+# TODO
+
+
+#######################################
+# The gradient of regularized batch gradient descent
 def compute_regularized_square_loss_gradient(X, y, theta, lambda_reg):
     """
-    Compute the gradient of L2-regularized average square loss function given X, y and theta
+    Compute the gradient of L2-regularized average
+    square loss function given X, y and theta
 
     Args:
-        X - the feature vector, 2D numpy array of size (num_instances, num_features)
+        X - the feature vector, 2D numpy array
+            of size (num_instances, num_features)
         y - the label vector, 1D numpy array of size (num_instances)
         theta - the parameter vector, 1D numpy array of size (num_features)
         lambda_reg - the regularization coefficient
@@ -230,11 +252,13 @@ def compute_regularized_square_loss_gradient(X, y, theta, lambda_reg):
     Returns:
         grad - gradient vector, 1D numpy array of size (num_features)
     """
-    #TODO
+    num_instances = X.shape[0]
+    return (1 / num_instances) * (X @ theta - y).T @ X\
+        + 2 * lambda_reg * theta.T
 
 
 #######################################
-### Regularized batch gradient descent
+# Regularized batch gradient descent
 def regularized_grad_descent(X,
                              y,
                              alpha=0.05,
@@ -242,57 +266,145 @@ def regularized_grad_descent(X,
                              num_step=1000):
     """
     Args:
-        X - the feature vector, 2D numpy array of size (num_instances, num_features)
+        X - the feature vector, 2D numpy array
+            of size (num_instances, num_features)
         y - the label vector, 1D numpy array of size (num_instances)
         alpha - step size in gradient descent
         lambda_reg - the regularization coefficient
         num_step - number of steps to run
-    
+
     Returns:
-        theta_hist - the history of parameter vector, 2D numpy array of size (num_step+1, num_features)
-                     for instance, theta in step 0 should be theta_hist[0], theta in step (num_step+1) is theta_hist[-1]
-        loss hist - the history of average square loss function without the regularization term, 1D numpy array.
+        theta_hist - the history of parameter vector, 2D numpy array
+                     of size (num_step+1, num_features) for instance,
+                     theta in step 0 should be theta_hist[0], theta in
+                     step (num_step+1) is theta_hist[-1]
+        loss hist - the history of average square loss function without
+                    the regularization term, 1D numpy array.
     """
-    num_instances, num_features = X.shape[0], X.shape[1]
-    theta = np.zeros(num_features)  #Initialize theta
-    theta_hist = np.zeros((num_step + 1, num_features))  #Initialize theta_hist
-    loss_hist = np.zeros(num_step + 1)  #Initialize loss_hist
-    #TODO
+    _, num_features = X.shape[0], X.shape[1]
+    # Initialize theta
+    theta = np.zeros(num_features)
+    # Initialize theta_hist
+    theta_hist = np.zeros((num_step + 1, num_features))
+    # Initialize loss_hist
+    loss_hist = np.zeros(num_step + 1)
+    for step_i in range(num_step + 1):
+        theta_hist[step_i, ] = theta
+        loss_hist[step_i] = compute_square_loss(X, y, theta)\
+            + lambda_reg * theta.T @ theta
+        theta -= alpha * compute_regularized_square_loss_gradient(X, y,
+                                                                  theta,
+                                                                  lambda_reg)
+    return theta_hist, loss_hist
+
+
+# choose best lambda
+"""
+lambda_regs = [1e-7, 1e-5, 1e-3, 1e-1, 1, 10 ,100]
+lambda_regs = np.linspace(np.exp(-9), np.exp(-3), 100)
+lambda_regs = np.linspace(np.exp(-9), np.exp(-2.2), 100)
+lambda_start = np.exp(-4.5) -> 0.0111
+"""
+
+
+def choose_lambda_reg(X_train, X_test, y_train, y_test,
+                      lambda_regs=0):
+    train_error = []
+    test_error = []
+    for lambda_reg in lambda_regs:
+        theta_hist, loss_hist = regularized_grad_descent(X_train,
+                                                         y_train,
+                                                         alpha=0.1,
+                                                         lambda_reg=lambda_reg)
+        train_error.append(loss_hist[-1])
+        test_error.append(compute_square_loss(X_test, y_test, theta_hist[-1]))
+    plt.plot(np.log(lambda_regs), train_error, 'y-', label='Training Error')
+    plt.plot(np.log(lambda_regs), test_error, 'r-', label='Test Error')
+    # plt.plot(lambda_regs, train_error, 'y-', label='Training Error')
+    # plt.plot(lambda_regs, test_error, 'r-', label='Test Error')
+    plt.legend()
+    plt.show()
+
+
+# get best theta
+def get_best_theta(X_train, y_train):
+    theta_hist, loss_hist = regularized_grad_descent(X_train,
+                                                     y_train,
+                                                     alpha=0.1,
+                                                     lambda_reg=np.exp(-4.5))
+    return theta_hist[-1], loss_hist[-1]
 
 
 #######################################
-### Stochastic gradient descent
+# Stochastic gradient descent
 def stochastic_grad_descent(X,
                             y,
-                            alpha=0.01,
-                            lambda_reg=10**-2,
+                            alpha=0.05,
+                            lambda_reg=np.exp(-4.5),
                             num_epoch=1000):
     """
-    In this question you will implement stochastic gradient descent with regularization term
+    In this question you will implement stochastic gradient descent
+    with regularization term
 
     Args:
-        X - the feature vector, 2D numpy array of size (num_instances, num_features)
+        X - the feature vector, 2D numpy array
+            of size (num_instances, num_features)
         y - the label vector, 1D numpy array of size (num_instances)
         alpha - string or float, step size in gradient descent
-                NOTE: In SGD, it's not a good idea to use a fixed step size. Usually it's set to 1/sqrt(t) or 1/t
-                if alpha is a float, then the step size in every step is the float.
+                NOTE: In SGD, it's not a good idea to use a fixed step size.
+                Usually it's set to 1/sqrt(t) or 1/t
+
+                if alpha is a float, then the step size
+                in every step is the float.
+
                 if alpha == "1/sqrt(t)", alpha = 1/sqrt(t).
                 if alpha == "1/t", alpha = 1/t.
         lambda_reg - the regularization coefficient
         num_epoch - number of epochs to go through the whole training set
 
     Returns:
-        theta_hist - the history of parameter vector, 3D numpy array of size (num_epoch, num_instances, num_features)
-                     for instance, theta in epoch 0 should be theta_hist[0], theta in epoch (num_epoch) is theta_hist[-1]
-        loss hist - the history of loss function vector, 2D numpy array of size (num_epoch, num_instances)
+        theta_hist - the history of parameter vector, 3D numpy array
+                     of size (num_epoch, num_instances, num_features)
+                     for instance, theta in epoch 0 should be theta_hist[0],
+                     theta in epoch (num_epoch) is theta_hist[-1]
+        loss hist - the history of loss function vector, 2D numpy array
+                    of size (num_epoch, num_instances)
     """
     num_instances, num_features = X.shape[0], X.shape[1]
-    theta = np.ones(num_features)  #Initialize theta
+    # Initialize theta
+    theta = np.ones(num_features)
+    # Initialize theta_hist
+    # Warn: We use 2D array here
+    theta_hist = np.zeros((num_epoch, num_features))
+    # Initialize loss_hist
+    # Warn: We use 1D array here
+    loss_hist = np.zeros(num_epoch)
+    for i in range(num_epoch):
+        theta_hist[i, ] = theta
+        loss_hist[i] = compute_square_loss(X, y, theta) +\
+            lambda_reg * (theta.T @ theta)
+        if alpha == '1/sqrt(t)':
+            step_size = 1.0 / np.sqrt(i + 1.0)
+        elif alpha == '1/t':
+            step_size = 1.0 / (i + 1.0)
+        else:
+            step_size = alpha
+        xi = X[np.random.randint(0, num_instances), ].reshape((-1, 1))
+        yi = y[np.random.randint(0, num_instances)]
 
-    theta_hist = np.zeros(
-        (num_epoch, num_instances, num_features))  #Initialize theta_hist
-    loss_hist = np.zeros((num_epoch, num_instances))  #Initialize loss_hist
-    #TODO
+        theta -= step_size * ((theta.T @ xi - yi) @ xi.T +
+                              4 * num_instances * lambda_reg * theta.T)
+    return theta_hist, loss_hist
+
+
+# choose step size
+# alphas = [0.05, 0.005, '1/t', '1/sqrt(t)']
+def plot_sgd_step_size(X, y, alphas):
+    for alpha in alphas:
+        _, loss_hist = stochastic_grad_descent(X, y, alpha)
+        plt.plot(np.log10(loss_hist), label=f"Alpha={alpha}")
+        plt.legend()
+    plt.show()
 
 
 def main():
@@ -315,7 +427,6 @@ def main():
         (X_train.shape[0], 1))))  # Add bias term
     X_test = np.hstack((X_test, np.ones(
         (X_test.shape[0], 1))))  # Add bias term
-    # TODO
 
 
 if __name__ == "__main__":
